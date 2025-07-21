0d0e3b261446e5e774331a2d4c3c89453e93e2f6
momenmust <200013269@must.edu.eg>
2025-07-22 02:40:31 +0300

    implement  Task Manager

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,15 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Setup successful")
+    val tasks = mutableListOf(
+        TaskManager("wash dishes", false), TaskManager("Walk", true)
+    )
+    tasks.add(TaskManager("get shower", true))
+    //for loop to iterate throw the tasks
+    for (task in tasks){
+        if (task.isDone)
+            println("✅ ${task.title}")
+        else
+            println("❌ ${task.title}")
+    }
 }
---
+++ src/main/kotlin/TaskManager.kt
@@ -0,0 +1,6 @@
+package org.example
+
+data class TaskManager(
+    val title: String,
+    val isDone: Boolean
+)
